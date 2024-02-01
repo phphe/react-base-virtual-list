9b8090552f60987598090c56a0be6ee2d696e08f
phphe <phphe@outlook.com>
2024-02-01 21:26:11 +0800

    chore: use unocss

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,7 @@
+// don't use reset css
+// import '@unocss/reset/tailwind.css'
+// import '@unocss/reset/tailwind-compat.css'
+import 'virtual:uno.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
